e9d56e5d5f1261dc70fbe40e48b7a7759a35b3b4
dmartinchick <dmartinchick@gmail.com>
2023-05-30 18:23:31 +0300

    created config class

--- config.py
+++ config.py
@@ -35,3 +35,35 @@ class SQLliteDbConfig(DbConfig):
 class MySQLDbConfig(DbConfig):
     def get_url(self):
         pass
+
+
+@dataclass
+class Proxies:
+    url: str
+
+
+class Config:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Config, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self, db_config: DbConfig, proxies: Proxies):
+        self.db_config = db_config
+        self.proxies = proxies
+
+    @property
+    def db_config(self):
+        return self.__db_config
+
+    @db_config.setter
+    def db_config(self, config: DbConfig):
+        self.__db_config = config
+
+    @property
+    def proxies(self):
+        return self.__proxies
+
+    @proxies.setter
+    def proxies(self, proxies: Proxies):
+        self.__proxies = proxies
